5250f29472a4479a49f06095cb4767dc982fbb5b
thaisonle94 <thaisonle94@gmail.com>
2014-08-27 03:44:37 +0700

    Upgraded to Meteor 0.9 + Fixed issue#1

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,11 @@
 Meteor.publish('rooms', function(){
   return Rooms.find();
 });
-Meteor.publish('messages', function(){
-  return Messages.find();
+Meteor.publish('rooms_by_id', function(_roomId){
+  return Rooms.find({_id: _roomId});
+});
+Meteor.publish('messages_by_room', function(_roomId){
+  console.log('log from Meteor publish messages 1 arg:');
+  console.log(_roomId);
+  return Messages.find({roomId: _roomId});
 });
